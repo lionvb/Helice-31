aeff856e75590ebeb804748af49bf948cafb1c48
NlsHub <nilscoudry@gmail.com>
2026-03-26 11:47:11 +0100

    j'espere que je vais pas niquer le gitHub

--- Software/Helice31/leds.cpp
+++ Software/Helice31/leds.cpp
@@ -5,7 +5,7 @@ Init
 majLeds(saturation)
 Met à jour toutes les LEDs : saturation moteur, mode numérique/analogique, RF actif
 Sortie
-setModeLed(mode)
+setModeSLed(mode)
 Force l'affichage du mode courant (NUMERIQUE / ANALOGIQUE / RF)
 Sortie
-*/```
+*/

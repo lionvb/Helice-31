2e3b3e2f65893d38b8516989ea73c1a5273f9f73
NlsHub <nilscoudry@gmail.com>
2026-04-02 11:32:14 +0200

    Update leds.cpp

--- Software/Helice31/leds.cpp
+++ Software/Helice31/leds.cpp
@@ -20,14 +20,14 @@ void initLeds() {
     pinMode(PIN_LED_MODE,       OUTPUT);
     pinMode(PIN_LED_RF,         OUTPUT);
     pinMode(PIN_LED_KP,         OUTPUT);
-    pinMode(PIN_LED_KI,         OUTPUT);
-    pinMode(PIN_LED_KD,         OUTPUT);
+    pinMode(PIN_LED_KI, OUTPUT);
+    pinMode(PIN_LED_KD, OUTPUT);
     digitalWrite(PIN_LED_SATURATION, LOW)
-    digitalWrite(PIN_LED_MODE,       LOW);
-    digitalWrite(PIN_LED_RF,         LOW);
-    digitalWrite(PIN_LED_KP,         LOW);
-    digitalWrite(PIN_LED_KI,         LOW);
-    digitalWrite(PIN_LED_KD,         LOW);
+    digitalWrite(PIN_LED_MODE, LOW);
+    digitalWrite(PIN_LED_RF, LOW);
+    digitalWrite(PIN_LED_KP, LOW);
+    digitalWrite(PIN_LED_KI, LOW);
+    digitalWrite(PIN_LED_KD, LOW);
     digitalWrite(PIN_LED_KP, HIGH);
 }
 

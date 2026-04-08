defbf4f16a9243f8b02daabe0c7285aabb8233c3
Victor BON <lionvb.616@gmail.com>
2026-04-08 18:30:12 +0200

    Rectification d'un nom de variable et rajout d'un point ;

--- Software/Helice31/leds.cpp
+++ Software/Helice31/leds.cpp
@@ -22,7 +22,7 @@ void initLeds() {
     pinMode(PIN_LED_KP, OUTPUT);
     pinMode(PIN_LED_KI, OUTPUT);
     pinMode(PIN_LED_KD, OUTPUT);
-    digitalWrite(PIN_LED_SATURATION,LOW)
+    digitalWrite(PIN_LED_SATURATION,LOW);
     digitalWrite(PIN_LED_MODE, LOW);
     digitalWrite(PIN_LED_RF, LOW);
     digitalWrite(PIN_LED_KP, LOW);
--- Software/Helice31/pid.cpp
+++ Software/Helice31/pid.cpp
@@ -14,12 +14,12 @@ void resetPID(EtatPID* pid) {
 float calculPID(float consigne, float mesure,float kp, float ki, float kd,EtatPID* pid){
     erreur=consigne-mesure; 
     Up=kp*erreur;   //Calcul Up
-    Ui=pid.integrale_prec+((ki*TE)/2)*(erreur-pid.erreur_prec);     //Calcul Ui
-    Ud=((TE*N)/(TE*N+2*(kd/kp)))*(pid.derivee_prec*((2*(kd/kp)/Te)+1)+(2*kd/Te)*(erreur-pid.erreur_prec)); //Calcul Ud
+    Ui=pid->integrale_prec+((ki*TE)/2)*(erreur-pid->erreur_prec);     //Calcul Ui
+    Ud=((TE*N_FILTRE)/(TE*N_FILTRE+2*(kd/kp)))*(pid->derivee_prec*((2*(kd/kp)/TE)+1)+(2*kd/TE)*(erreur-pid->erreur_prec)); //Calcul Ud
     Un=Up+Ui+Ud;    //Calcul total
-    pid.erreur_prec=erreur;
-    pid.integrale_prec=Ui;
-    pid.derivee_prec=Ud;
+    pid->erreur_prec=erreur;
+    pid->integrale_prec=Ui;
+    pid->derivee_prec=Ud;
     return Un;
 }
 
